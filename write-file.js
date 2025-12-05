68fad32eee6a4de1acdd0769c9b54f9fc8352dd5
Debajit91 <debajitroy544@gmail.com>
2025-12-05 19:31:05 +0600

    explored write file

---
+++ write-file.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+const content1 = " This is a content \n nodejs is awesome!!!";
+
+try {
+    fs.writeFileSync("./output/test-sync.txt", content1);
+    console.log("file written sync");
+} catch (error) {
+    console.error(error.message);
+}
+
+const content2 = " This is a content two \n asynchronously!!!";
+
+fs.writeFile("./output/test-async.txt", content2, (error) =>{
+    if(error){
+        console.error(error.message);
+    } else {
+        console.log("file written asynchronously");
+    }
+});
