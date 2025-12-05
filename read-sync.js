5b31dfaf643453bad717c3c875ad2a34f4327e93
Debajit91 <debajitroy544@gmail.com>
2025-12-05 19:10:48 +0600

    explored reading files with fs module

---
+++ read-sync.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+console.log("start reading...");
+
+try {
+  const data = fs.readFileSync("data/diary.txt", "utf-8");
+  console.log(data);
+} catch (err) {
+    console.error(error.message)
+}
+
+console.log("finished");
