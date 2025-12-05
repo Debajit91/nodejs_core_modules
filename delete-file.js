94e1358bf70e3bbee44d3b2a58a8d4380425013b
Debajit91 <debajitroy544@gmail.com>
2025-12-05 20:27:22 +0600

    explored delete file system async

--- delete-file.js
+++ delete-file.js
@@ -3,16 +3,29 @@ const fs = require("fs");
 fs.writeFileSync("./output/temp.txt", "this is a temp file");
 console.log("file created");
 
-// if (fs.existsSync("./output/temp.txt")){
-//     console.log("file exists!!!");
+if (fs.existsSync("./output/temp.txt")){
+    console.log("file exists!!!");
 
-//     fs.unlinkSync("./output/temp.txt");
-//     console.log("file deleted");
-// }
+    fs.unlinkSync("./output/temp.txt");
+    console.log("file deleted");
+}
 
 
-// try {
-//    fs.unlinkSync("./output/temp.txt"); 
-// } catch (error) {
-//     console.log("ERROR :", error.message);
-// }
+try {
+   fs.unlinkSync("./output/temp.txt"); 
+} catch (error) {
+    console.log("ERROR :", error.message);
+}
+
+
+fs.writeFile("./output/temp2.txt", "Another Temp File", (err) =>{
+    console.log("Another Temp File Created");
+
+    fs.unlink("./output/temp2.txt", (err) =>{
+        if(err){
+            console.error("ERROR :", err.message);
+        } else {
+            console.log("Temp2 deleted")
+        }
+    });
+});
